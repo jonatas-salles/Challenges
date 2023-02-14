c0448d170ae4320bc409e3c0e58643e416f35c38
jonatas-salles <jonatas.salles@outlook.com>
2023-02-14 19:25:14 -0300

    :rocket: Day 6
    Signed-off-by: jonatas-salles <jonatas.salles@outlook.com>

---
+++ 100DaysOfCode/Day 6/main.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.DataFrame()
+df = pd.read_csv(r'Day 6\NetflixOriginals.csv', encoding_errors='ignore')
+index = df.index
+
+def getGenre(genero):
+    global genero_list, score
+    score = 0
+    condition = df["Genre"] == genero
+    genero_indices = index[condition]
+    genero_list = genero_indices.to_list()
+
+    for item in genero_list:
+        print(df.loc[item])
+        if df.loc[item, 'IMDB Score'] > score:
+            score = df.loc[item, 'IMDB Score']
+        print('-'*30)
+
+getGenre("Comedy")
+percent = (len(genero_list) * 100) / len(index)
+print(f'O genero "Comedy" representa {percent:.0f}% do catalogo')
+print(f'O maior score do IMDB desse genero é {score}')
