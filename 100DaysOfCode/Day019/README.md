742af4591d4a7e74dc781490757e43b88e90b97e
jonatas-salles <jonatas.salles@outlook.com>
2023-02-27 19:52:52 -0300

    :rocket: Day 19
    Signed-off-by: jonatas-salles <jonatas.salles@outlook.com>

---
+++ 100DaysOfCode/Day019/README.md
@@ -0,0 +1,3 @@
+# U.S. States guessing game
+
+Type exit to quit the game and get a csv file with missing states.
---
+++ 100DaysOfCode/Day019/main.py
@@ -0,0 +1,36 @@
+import turtle
+import pandas as pd
+
+image = r"100DaysOfCode\Day019\blank_states_img.gif"
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.addshape(image)
+turtle.shape(image)
+
+data = pd.read_csv(r"100DaysOfCode\Day019\50_states.csv")
+all_states = data["state"].to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("100DaysOfCode\Day019\learn.csv", index=False, header=False)
+
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data["state"] == answer_state]
+        t.goto(int(state_data["x"]), int(state_data["y"]))
+        t.write(answer_state)
+
+turtle.mainloop()
