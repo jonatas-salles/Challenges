c4daeb07d442539be5e5f7f843e093e781805d86
jonatas-salles <jonatas.salles@outlook.com>
2023-03-04 17:49:17 -0300

    feat: add key error treatment
    Signed-off-by: jonatas-salles <jonatas.salles@outlook.com>

--- 100DaysOfCode/Day020/main.py
+++ 100DaysOfCode/Day020/main.py
@@ -4,7 +4,14 @@ nato_df = pandas.read_csv(r"100DaysOfCode\Day020\nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter:row.code for (index, row) in nato_df.iterrows()}
 
-word = input("Enter a word: ").upper()
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output)
 
-output = [nato_dict[letter] for letter in word]
-print(output)
+generate_phonetic()
